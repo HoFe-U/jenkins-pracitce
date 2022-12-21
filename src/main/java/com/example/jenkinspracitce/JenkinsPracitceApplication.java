8493049544bc8f4e42c2bbfccbf1dc071fc327d8
HoFe-U <hochul-y@naver.com>
2022-12-21 16:26:55 +0900

    Initial commit

---
+++ src/main/java/com/example/jenkinspracitce/JenkinsPracitceApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinspracitce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPracitceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsPracitceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkinspracitce/JenkinsPracitceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinspracitce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPracitceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
