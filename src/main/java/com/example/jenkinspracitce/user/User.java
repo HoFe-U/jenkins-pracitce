25c8176f2934ff4f530f4a4ec4dc6c86c171b886
HoFe-U <hochul-y@naver.com>
2022-12-23 13:52:56 +0900

    [FEAT] : test

---
+++ src/main/java/com/example/jenkinspracitce/user/User.java
@@ -0,0 +1,10 @@
+package com.example.jenkinspracitce.user;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String test;
+}
